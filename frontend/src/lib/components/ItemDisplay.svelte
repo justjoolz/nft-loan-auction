4d0ca8bd7ffab5e76b5a1faf063d92c6bda68394
grant@bemedia.com.au <grant@bemedia.com.au>
2023-07-17 08:42:55 +0200

    update request modal layout

--- frontend/src/lib/components/ItemDisplay.svelte
+++ frontend/src/lib/components/ItemDisplay.svelte
@@ -3,7 +3,6 @@
 	import ItemCard from './ItemCard.svelte';
 
 	export let nfts: NFTCatalogEntry[][];
-	export let imgSrc: string;
 	export let pageTitle: string;
 
 	let folder = true;
@@ -14,28 +13,19 @@
 	console.log(nfts, 'nfts');
 </script>
 
-<div class="flex flex-col lg:h-full pb-14 px-10 w-full lg:w-1/2">
-	<div class="w-full flex items-center gap-x-3 pb-6">
-		<img src={imgSrc} alt="" class="w-8 pb-1" />
+<div class="flex flex-col px-10 w-full lg:w-1/2">
+	<div class="w-full flex items-center gap-x-3">
 		<p class="text-3xl font-b7">{pageTitle}</p>
 	</div>
-	<div class="border-l-2 border-b-2 border-primary-500 relative mt-4">
-		<img
-			src={topFolder}
-			alt=""
-			class="w-full absolute top-[-6px] sm:top-[-8px] md:top-[-12px] lg:top-[-7px] xl:top-[-12px]"
-		/>
+	<div class="relative">		
 		<div
-			class="absolute top-[-10px] sm:top-[-10px] md:top-[-13px] lg:top-[-12px] xl:top-[-15px] left-[10%] w-[22%] py-2"
-		/>
-		<div
-			class="w-full py-8 lg:py-6 flex flex-col gap-y-4 border-r-2 border-primary-500 mt-[4%] sm:mt-[24px] md:mt-[32px] lg:mt-[20px] xl:mt-[29px] lg:min-h-[100vh]"
+			class="w-full py-8 lg:py-6 flex flex-col gap-y-4"
 		>
 			{#each nfts as nftCollection}
 				{#if nftCollection[0]?.collectionName !== undefined}
 					<div>
-						<p class="h3 font-bold px-4">{nftCollection[0]?.collectionName}</p>
-						<div class="gridDisplay p-4">
+						<p class="h3 font-bold">{nftCollection[0]?.collectionName}</p>
+						<div class="gridDisplay py-4">
 							{#each nftCollection as nft}
 								<ItemCard {nft} />
 							{/each}
--- frontend/src/lib/components/Modals/CreateLoanModal.svelte
+++ frontend/src/lib/components/Modals/CreateLoanModal.svelte
@@ -44,18 +44,38 @@
 </script>
 
 {#if $modalStore[0]}
-	<button class="btn-icon variant-filled {cButton}" on:click={parent.onClose}>×</button>
-	<div class="card flexRowCenter p-40">
+	<button class="btn-icon variant-filled {cButton}" on:click={parent.onClose}>X</button>
+	<div
+		class="card p-6 md:p-10 variant-filled-tertiary min-w-[90%] md:min-w-4/5 min-h-[90%] md:min-h-4/5 flex items-center justify-center gap-4"
+	>
 		<ContentDisplay pageTitle="Select NFT to use as collateral" />
 
-		<label for="duration" class="label">Duration (days)</label>
-		<input bind:value={duration} type="number" placeholder="Duration" class="input" />
-
-		<label for="yield" class="label">Yield (%)</label>
-		<input bind:value={_yield} type="number" placeholder="Yield" class="input" />
-
-		<button class="btn variant-filled-primary font-bold" on:click={handleCreateLoanAuctionClick}
-			>Create New Loan Request</button
-		>
+		<div class="flex flex-col w-1/3 gap-6">
+			<div class="flex flex-col w-full">
+				<label for="duration" class="font-bold pb-2">Duration (days)</label>
+				<input
+					type="number"
+					id="duration"
+					class="input w-full"
+					placeholder="Duration"
+					bind:value={duration}
+					tabindex="0"
+				/>
+			</div>
+			<div class="flex flex-col w-full">
+				<label for="yield" class="font-bold pb-2">Pay back USDC</label>
+				<input
+					type="number"
+					id="yield"
+					class="input w-full"
+					placeholder="Yield"
+					bind:value={_yield}
+					tabindex="0"
+				/>
+			</div>
+			<button class="btn variant-filled-primary font-bold" on:click={handleCreateLoanAuctionClick}
+				>Create New Loan Request</button
+			>
+		</div>
 	</div>
 {/if}
